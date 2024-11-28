1e70cbff43cb1ee3a02c571628b94bc07bbfef3b
Kewsen <kobusevdanila@gmail.com>
2024-11-28 17:57:59 +0300

    Сделал архитектуру базы данных

--- src/db.py
+++ src/db.py
@@ -1,1 +1,16 @@
 """Файл с классом Warehouse"""
+
+
+class Warehouse:
+
+    def __init__(self, db_name='warehouse.db'):
+        pass
+
+    def get_staff(self):
+        pass
+
+    def write_stuff(self, stuff):
+        pass
+
+    def order_ingredients(self, ingredients):
+        pass
