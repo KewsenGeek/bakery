8298484a24d42d6c2c354065fbc3dc94ae694ca4
Arfit1 <61419503+Arfit1@users.noreply.github.com>
2024-12-01 14:07:03 +0300

    Update buyer.py

--- src/buyer.py
+++ src/buyer.py
@@ -1,32 +1,38 @@
 """Файл с классом buyer"""
 class buyer:
 
-    def __init__(self, name, age, money, sex, address):
+    def __init__(self, name=str, age=int, money=int, sex=bool, address=str):
         self.name = name
         self.age = age
         self.money = money
         self.sex = sex
         self.address = address
-#сами функции пропишу после появления каталога
+
 
     """функция проверки хватает ли денег"""
-    def check_money(money,price):
-        price = 10
-        if money < price:
+    #на ввод подаётся цена товара. сравнивает кол-во денег клинта и цену товара
+    def check_money(self,price):
+        
+        if self.money < price:
            print("Найди работу")
         else:
             print("приятно иметь с вами дело")
-        #сравнить деньги покупателя с ценой товара, при нехватке отменить заказ и вывыести ответ покупателю с предложением устроиться на работу
+        
+    
     """функция проверки достаточно ли взрослый человек"""
-    def check_age(age, required_age):
-        required_age = 18
-        if age < required_age:
-            print( "вохвращайтесь через", required_age - age, "лет")
+    #на ввод подаётся возрастная категория товара (кол-во лет, необходиоме для покупки) сравниавет с возрастом заказчика
+    def check_age(self, required_age):
+        
+        
+        if self.age < required_age:
+            print( "возвращайтесь через", required_age - self.age, "лет")
         else:
             print("приятно иметь с вами дело")
             
         
-        #сравнить категорию товара по вохрасту и возраст покупатля. Если покупатель младше, 
-        #то вывести ответ с предложением вернуться через "возрастная категория товара минус возраст покупателя" лет
+       
     
-  
+#k = buyer(input("имя"),input("возраст"),input("деньги"),input("пол"),input("адрес"))
+#k = buyer ("зер", 13,500, "муж", "ван")
+#k.check_age(18)
+#k.check_money(10000)
