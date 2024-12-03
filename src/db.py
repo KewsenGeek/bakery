86dd5bb448f4d320d538a644b5011041a7f17690
Kewsen <kobusevdanila@gmail.com>
2024-12-03 18:50:11 +0300

    убрал список товаров

--- src/db.py
+++ src/db.py
@@ -8,10 +8,6 @@ class Warehouse:
 
     def __init__(self, db_name='warehouse.db'):
 
-        self.all_pastry = [
-            'Круассаны', 'Розаны', 'Печенье', 'Хлеб', 'Багет'
-        ]
-
         self.connection = sqlite3.connect(db_name)
         self.cursor = self.connection.cursor()
         self.cursor.execute(
